dec198249c4ea68f75630e50e4d16a3e7a220ad4
Ryan Shuhart <shuhart@gmail.com>
2017-04-08 14:58:00 -0500

    Clean up and added color to generation. Add Shiny Notebook

--- src/familyTreePlot.R
+++ src/familyTreePlot.R
@@ -68,6 +68,7 @@ exptOne = function(l, k, mG, mO){
 draw_tree <- function(lambda = .4, kappa = 1, maxGen = 10, seedx=12062013){
   library(dplyr)
   library(ggplot2)
+  #lambda = .4; kappa = 1; maxGen = 10; seedx=12062013
   set.seed(seedx); tree <- familyTree(lambda=lambda, kappa = kappa, maxGen = maxGen)
   
   tree_df <- data.frame()
@@ -80,40 +81,39 @@ draw_tree <- function(lambda = .4, kappa = 1, maxGen = 10, seedx=12062013){
   }
   
   # Add y cord
-  #tree_df$y_cord <- 1:nrow(tree_df)*.5
-  tree_df$y_cord <- c(0.0, -1.0, .5, -.5, -1.5, 1.5, -2.0, 1.0, 2.0)
+  #tree_df$x_cord <- 1:nrow(tree_df)*.5
+  tree_df$x_cord <- c(0.0, -1.0, .5, -.5, -1.5, 1.5, -2.0, 1.0, 2.0)
   
   # Add parent generation reference
   tree_df$Parent_Gen <- tree_df$Gen-1
   
   # Self join the table to identify the births on the parent
-  marks <- inner_join(select(tree_df, kidID, Gen, y_cord), 
+  marks <- inner_join(select(tree_df, kidID, Gen, x_cord), 
                       select(tree_df, parentID, Parent_Gen, births), 
                       by=c("kidID" = "parentID", "Gen"="Parent_Gen"))
   
   # Connect birth to life line
-  conn_lines <- inner_join(select(tree_df, births, y_cord),
-                           select(marks, births, y_cord), by="births")
+  conn_lines <- inner_join(select(tree_df, births, x_cord, Gen),
+                           select(marks, births, x_cord), by="births")
   
   # Generation separation locations
   gen_lines <- tree_df %>% 
     group_by(Gen) %>% 
-    summarize(Gen_Break = max(y_cord)+.25) %>%
+    summarize(Gen_Break = max(x_cord)+.25) %>%
     mutate(Gen_Label = paste("Gen", Gen))
   
   g <- ggplot(tree_df) + 
-    geom_segment(aes(y = -births, x = y_cord, yend = -completes, xend = y_cord), color="#7B7F7B", size=1.25) +
-    geom_segment(aes(y = -births, x = y_cord.x, yend = -births, xend = y_cord.y), data = conn_lines, color="#7B7F7B", size=1.25) +
-    geom_point(aes(y = -births, x = y_cord), data = marks, shape=19, size=3, color="#007F00") +
-    #geom_hline(aes(yintercept=Gen_Break), data = gen_lines, linetype = 2) +
-    #scale_y_continuous(labels=gen_lines$Gen_Label, breaks=gen_lines$Gen_Break) + 
+    geom_segment(aes(y = -births, x = x_cord, yend = -completes, xend = x_cord, color=as.factor(Gen-1)), size=1.25) +
+    geom_segment(aes(y = -births, x = x_cord.x, yend = -births, xend = x_cord.y, color=as.factor(Gen-1)), data = conn_lines,  size=1.25) +
+    geom_point(aes(y = -births, x = x_cord), data = marks, shape=19, size=3, color="#007F00") +
+    scale_color_discrete(name="Generation")+ 
     theme_bw() + 
     labs(y = "Time") +
     theme(axis.title.y=element_blank(),
           panel.grid.major.y = element_blank(),
           panel.grid.minor.y = element_blank(),
           panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank())
+          panel.grid.minor.x = element_blank()) 
   
   return(g)
 }
